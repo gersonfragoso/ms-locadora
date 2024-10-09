3bd79e37c151ae6353442bf70fe101ff6acf5f9e
gerson.fragoso <gersonfragoso.araujo@gmail.com>
2024-10-09 10:51:34 -0300

    config initial

---
+++ customer-service/src/test/java/com/solutis/locadora/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutis.locadora.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ vehicle-catalog/src/test/java/com/solutis/locadora/vehicle_catalog/VehicleCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutis.locadora.vehicle_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
