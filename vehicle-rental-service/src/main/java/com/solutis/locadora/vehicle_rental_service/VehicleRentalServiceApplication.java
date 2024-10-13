d9d7520e5c81c1a29fc03390e25b3015653bf77c
gerson.fragoso <gersonfragoso.araujo@gmail.com>
2024-10-13 13:48:06 -0300

    feat: add microservice vehicle-rental-service

---
+++ vehicle-rental-service/src/main/java/com/solutis/locadora/vehicle_rental_service/VehicleRentalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.solutis.locadora.vehicle_rental_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRentalServiceApplication.class, args);
+	}
+
+}
---
+++ vehicle-rental-service/src/test/java/com/solutis/locadora/vehicle_rental_service/VehicleRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutis.locadora.vehicle_rental_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
