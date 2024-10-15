70f02e06099a3e5c53c1f1aaf4d1261cb6d09552
Maria Eduarda <mariaeduardasoter@gmail.com>
2024-10-15 16:38:53 -0300

    update

--- customer-service/src/main/java/com/solutis/locadora/customer_service/driver/DriverRecordDto.java
+++
@@ -1,7 +0,0 @@
-package com.solutis.locadora.customer_service.driver;
-
-import com.solutis.locadora.customer_service.person.PersonRecordDto;
-
-public record DriverRecordDto(String licenseNumber,
-                              PersonRecordDto person) {
-}
