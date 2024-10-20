b768d53a854c7c88e14f19fa43352b500d41e6e1
gerson.fragoso <gersonfragoso.araujo@gmail.com>
2024-10-20 17:35:10 -0300

    feat: config docker by vehicle-rental-service

--- vehicle-catalog/src/main/java/com/solutis/locadora/vehicle_catalog/VehicleCatalogApplication.java
+++ vehicle-catalog/src/main/java/com/solutis/locadora/vehicle_catalog/VehicleCatalogApplication.java
@@ -3,8 +3,10 @@ package com.solutis.locadora.vehicle_catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableFeignClients
 public class VehicleCatalogApplication {
 
 	public static void main(String[] args) {
